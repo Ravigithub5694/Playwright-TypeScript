becb90ba6a32ce956593e8359df16347d2d445c5
RaviKumar <cravi.ravikumar1@gmail.com>
2026-01-02 13:44:18 +0530

    Jan 2026 2nd commit two

--- tests/Concepts2/AuthenticatedPopup.spec.ts
+++ tests/Concepts2/AuthenticatedPopup.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from '@playwright/test';
 // This is for handling authenticated Popup concept
 test("Authenticated Popup handling @AuthPopup", async ({ browser }) => {
-    const context = await browser.newContext();
+   // const context = await browser.newContext(); Approch 1
+   //Aproach 2  using browser context
+   const context = await browser.newContext({httpCredentials:{username:'admin',password:'admin'}})
     const page = await context.newPage()
-    // await page.goto("https://the-internet.herokuapp.com/basic_auth")
-    await page.goto("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+    await page.goto("https://the-internet.herokuapp.com/basic_auth")
+   // await page.goto("https://admin:admin@the-internet.herokuapp.com/basic_auth")
     //Multiple popups handling
     await page.waitForLoadState('networkidle')
     await page.waitForTimeout(3000)
     await expect(page.getByText("Congratulations! You must have the proper credentials.")).toBeVisible()
+
+
 })
---
+++ tests/Concepts2/Autowaiting.spec.ts
@@ -0,0 +1,6 @@
+import {test,expect} from '@playwright/test';
+// This is Auto waiting concepts
+test("Auto waiting concepts @Autowait", async ({page}) => {
+    await page.goto("https://demowebshop.tricentis.com/")
+    
+})
